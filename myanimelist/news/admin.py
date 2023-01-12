2b7531807a6ac219baea4dacacb64beb3598b014
maulanawesome5 <maulanawesome5@gmail.com>
2023-01-13 03:24:19 +0700

    Membuat model database, serializer, dan routing

--- myanimelist/news/admin.py
+++ myanimelist/news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import News
+
 
 # Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug', 'publish', 'updated']
+
+admin.site.register(News, NewsAdmin)
--- myanimelist/news/views.py
+++ myanimelist/news/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NewsSerializer
+from .models import News
+
+
+method_get = 'GET'
+method_post = 'POST'
+method_update = 'PUT'
+method_delete = 'DELETE'
 
 # Create your views here.
+@api_view([method_get])
+def getNews(request):
+    news = News.objects.all()
+    serializer = NewsSerializer(news, many=True)
+    return Response(serializer.data)
+
+@api_view([method_get])
+def detailContent(request, pk, slug):
+    """
+    Funtion ini untuk menampilkan konten News berdasarkan URL
+    seperti http://nama_domain.com/news/1/judul-konten-berita
+    """
+    news = News.objects.get(id=pk, slug=slug)
+    serializer = NewsSerializer(news, many=False)
+    return Response(serializer.data)
+
+@api_view([method_update])
+def updateNews(request, pk):
+    """Function ini masih dalam tahap development. Belum bisa dipakai"""
+    data = request.data
+    news = News.objects.get(id=pk)
+    serializer = NewsSerializer(news, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view([method_delete])
+def deleteNews(request, pk):
+    """
+    Function ini untuk menghapus data berdasarkan primary key data News
+    URL yang dipakai http://nama_domain.com/news/1/delete
+    """
+    news = News.objects.get(id=pk)
+    news.delete()
+    return Response("Berita telah dihapus!")
