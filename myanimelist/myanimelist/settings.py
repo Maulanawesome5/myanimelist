8a33c249a0589856297c878badf5efc75f0fa098
maulanawesome5 <maulanawesome5@gmail.com>
2023-02-09 01:18:29 +0700

    Connect ke xampp mysql. Docker mysql failed conect

--- myanimelist/myanimelist/settings.py
+++ myanimelist/myanimelist/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'myanimelist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myanimelist_indonesia',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
