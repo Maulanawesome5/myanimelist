bc2e058fd405e677a382d28d1f711b57bbbe6925
maulanawesome5 <maulanawesome5@gmail.com>
2023-02-08 23:44:10 +0700

    Hapus django apps animelist, api, news

--- myanimelist/animelist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Anime
-
-
-# Register your models here.
-class AnimeAdmin(admin.ModelAdmin):
-    readonly_fields = ["slug",]
-
-admin.site.register(Anime, AnimeAdmin)
--- myanimelist/animelist/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Anime
-
-
-class AnimeSerializer(ModelSerializer):
-    class Meta:
-        model = Anime
-        fields = "__all__"
