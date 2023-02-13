821c74f81661f01913664f52c280b6f5ae1765f1
maulanawesome5 <maulanawesome5@gmail.com>
2023-02-13 12:27:52 +0700

    Membuat environment variabel

--- myanimelist/myanimelist/settings.py
+++ myanimelist/myanimelist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +79,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myanimelist_indonesia',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'NAME': "myanimelist_indonesia",
+        'USER': os.getenv("MARIADB_USER"),
+        'PASSWORD': "Mandala281",
+        'HOST': "localhost",
+        'PORT': os.getenv("MARIADB_DATABASE_PORT"),
         'OPTIONS': {
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'",
         },
@@ -127,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
